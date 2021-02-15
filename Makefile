de05f34e10ffb920df3d9dcc822405d9dd31e055
userse31 <61165364+userse31@users.noreply.github.com>
2021-02-14 20:39:22 -0500

    Better!
    Play field can be resized to better fit your terminal. Use switches "-w" and "-h" to use the feature.
    
    WANDS!!!! Collect wands from enemies holding a "¥". Use a wand by pressing enter. This rebuilds the wall

--- Makefile
+++ Makefile
@@ -6,6 +6,14 @@ CFLAGS=-lcurses
 #	$(CC) -c -o $@ $< $(CFLAGS)
 
 mainfile: numattack.c
-	$(CC) -o numattack numattack.c $(CFLAGS)
+	$(CC) -o numattack numattack.c -w $(CFLAGS)
 
-$(info ignore the warnings...)
+install:
+	mkdir /opt/numattack
+	cp ./numattack /opt/numattack/
+	ln /opt/numattack/numattack /usr/games/numattack
+uninstall:
+	- rm -f /usr/games/numattack
+	- rm -rf /opt/numattack/
+clean:
+	- rm ./numattack
--- numattack.c
+++ numattack.c
@@ -1,4 +1,4 @@
-/* Copyright 2019
+/* Copyright 2019 Noah M. Walker - All Rights Reserved
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -12,27 +12,54 @@
 #include <time.h>
 #include <math.h>
 
-int wall[7];
+int *wall;
 int score=0;
+//enemy
 int enemyx;
 int enemyy;
 int enemyval;
+bool has_wand;
 int play=1;
 int enemydelay=5000;
 int tmp;
 int counter;
 int level=1;
+int wands=0;
 bool debug=0;
 WINDOW *canvas;
 time_t startstamp;
 time_t endstamp;
+int width=22;
+int height=7;
 int main(int argc, char *argv[])
 {
-if(argc==2){
+printf("[10;100][11;50]");
+if(argc>1){
 if(!strcmp(argv[1],"-d")){
 debug=1;
 }
+for(int i=1;i<argc;i++){
+//printf("%s\n",argv[i]);
+	if(!strcmp(argv[i],"-d")){
+	debug=1;
+	}
+	if(!strcmp(argv[i],"-w")){
+		if(argc<i+1){
+		return 0;
+		}
+	width=atoi(argv[i+1]);
+	}
+	if(!strcmp(argv[i],"-h")){
+		if(argc<i+1){
+		return 0;
+		}
+	height=atoi(argv[i+1]);
+	}
 }
+}
+//printf("%i,%i\n",width,height);
+//exit(0);
+wall=malloc(sizeof(int)*height);
 printf("(U");
 startstamp=time(NULL);
 srand(time(NULL));
@@ -44,12 +71,14 @@ nodelay(stdscr,TRUE);
 curs_set(0);
 newenemy();
 //main loop
+draw_wall();
 while(play){
 enemyx--;
 draw();
 refresh();
 if(enemyx==0){
-draw();
+//draw();
+draw_wall();
 beep();
 if(wall[enemyy]==0){
 wall[enemyy]=1;
@@ -74,68 +103,89 @@ printf("You have been playing for %d seconds\n",endstamp-startstamp);
 printf("Thank you for playing [36m[42mNumber Attack![37m[40m\n");
 return 0;
 }
-int draw(){
+int draw_wall(){
 clear();
-for(int a=0;a<7;a++){
+for(int a=0;a<height;a++){
 move(a,0);
-if(wall[a]==0){
-printw("#");
-}else{
-printw(" ");
+	if(wall[a]==0){ //draw wall
+	printw("#");
+	}else{
+	printw(" ");
+	}
 }
-}
-move(enemyy,enemyx);
-printw("%d",enemyval);
-mvprintw(7,0,"score: %d",score);
+mvprintw(height,0,"score: %d level: %d wands: %d",score,level,wands);
 if(debug){
-mvprintw(8,0,"enemy delay: %d", enemydelay);
+mvprintw(height+1,0,"enemy delay: %d", enemydelay);
+}
+//draw();
+}
+int draw(){
+move(enemyy,enemyx);
+if(has_wand){
+printw("%d¥ ",enemyval);
+}else{
+printw("%d ",enemyval);
 }
 }
 int newenemy(){
 enemyval=(rand()%10);
-enemyx=22;
-enemyy=(rand()%7);
+enemyx=width;
+enemyy=(rand()%height);
+has_wand=0;
+if(!(rand()%(level*10))){ //give enemy wand depending on level.
+has_wand=1;
+}
 }
 int attack(){
 if((tmp==48)&&(enemyval==0)){ //winning
+draw_wall();
 score++;
+if(has_wand){
+wands++;
+}
 beep();
 if((score%10)==0){
 level++;
-}
-if(enemydelay>1000){
-enemydelay-=100;
+if(enemydelay>1000){ //speed things up on new level
+enemydelay-=200;
 }else{
 enemydelay=1000;
 }
+}
 newenemy();
 }
-if((tmp==49)&&(enemyval==1)){
+if((tmp==49)&&(enemyval==1)){//1
 enemyval--;
 }
-if((tmp==50)&&(enemyval==2)){
+if((tmp==50)&&(enemyval==2)){//2
 enemyval--;
 }
-if((tmp==51)&&(enemyval==3)){
+if((tmp==51)&&(enemyval==3)){//3
 enemyval--;
 }
-if((tmp==52)&&(enemyval==4)){
+if((tmp==52)&&(enemyval==4)){//4
 enemyval--;
 }
-if((tmp==53)&&(enemyval==5)){
+if((tmp==53)&&(enemyval==5)){//5
 enemyval--;
 }
-if((tmp==54)&&(enemyval==6)){
+if((tmp==54)&&(enemyval==6)){//6
 enemyval--;
 }
-if((tmp==55)&&(enemyval==7)){
+if((tmp==55)&&(enemyval==7)){ //7
 enemyval--;
 }
-if((tmp==56)&&(enemyval==8)){
+if((tmp==56)&&(enemyval==8)){ //8
 enemyval--;
 }
-if((tmp==57)&&(enemyval==9)){
+if((tmp==57)&&(enemyval==9)){ //key 9
 enemyval--;
+}
+if((tmp==10)&&(wands>0)){ //use wand: enter key
+for(int i=0;i<7;i++){
+wall[i]=0;
+}
+wands--;
 }
 draw();
 }
